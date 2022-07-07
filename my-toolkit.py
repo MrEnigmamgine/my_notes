b25644733f290d8bc16bb219fb885e2f60fb663d
MrEnigmamgine "Joshua Mayes <mayes.joshua.d@gmail.com>
2022-07-07 14:00:38 -0400

    push

--- my-toolkit.py
+++ my-toolkit.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 import datetime
+from pydataset import data
+
+# SKLearn
+import sklearn as sk
+import sklearn.model_selection as skm
 
 
 '''  get_db_url()
@@ -34,6 +39,11 @@ def get_db_url(database, username='', password='', hostname='', env=''):
         password = d['password']
     url = f'mysql+pymysql://{username}:{password}@{hostname}/{database}'
     return url
+
+# Easily load a google sheet (first tab only)
+def read_google(url):
+    csv_export_url = url.replace('/edit#gid=', '/export?format=csv&gid=')
+    return pd.read_csv(csv_export_url)
 
 
 # Return a float from a string representing a usd amount.
@@ -65,3 +75,22 @@ def chi2_test(df, alpha=0.05):
         print("We reject the null hypothesis")
     else:
         print("We fail to reject the null hypothesis")
+
+
+## Generic split data function
+def train_validate_test_split(df, seed=123, stratify=None):
+    # First split off our testing data.
+    train_and_validate, test = skm.train_test_split(
+        df, 
+        test_size=0.2, 
+        random_state=seed, 
+        stratify=( df[stratify] if stratify else None)
+    )
+    # Then split the remaining into train/validate data.
+    train, validate = skm.train_test_split(
+        train_and_validate,
+        test_size=0.3,
+        random_state=seed,
+        stratify= (train_and_validate[stratify] if stratify else None)
+    )
+    return train, validate, test
